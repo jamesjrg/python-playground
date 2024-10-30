51fb7240547812053d0f7be2ac22cd8c768c77e2
James Gregory <james.jrg@gmail.com>
2024-10-30 22:40:48 +0000

    reorganise repo, and add a couple more tiny LLM projects

---
+++ README.md
@@ -0,0 +1,5 @@
+Install steps shared by all projects in this repo:
+
+1. install Python 3.11 (e.g. pyenv or asdf)
+2. install direnv
+3. `pre-commit install`
--- uses_llm/claude_iterate/iterate.py
+++ uses_llm/claude_iterate/iterate.py
@@ -11,14 +11,15 @@ def command(file, prompt, sonnet):
         model="claude-3-5-sonnet-20240620"
     else:
         model = "claude-3-haiku-20240307"
-        
+
     llm = ChatAnthropic(
-    model=model,
-    max_tokens=4096)    
-    
+        model=model,
+        max_tokens=4096
+    )
+
     with open(file) as f:
         content = f.read()
-        
+
     full_prompt = content + "\n" + prompt
     print(full_prompt)
     messages = [
@@ -27,16 +28,16 @@ def command(file, prompt, sonnet):
     ]
     response = llm.invoke(messages)
     print(response.content)
-    
+
     with NamedTemporaryFile(delete_on_close=False) as fp:
         f.write(response.content)
-        
+
         # todo: display git diff, then optionally write to other file
-    
+
     with open(file, "w") as f:
         f.write(response.content)
-        
-    
-    
+
+
+
 if __name__ == '__main__':
     command()
